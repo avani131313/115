01717ff29e9d7a0e2f365da1e18952be544eba61
avani131313 <97681549+avani131313@users.noreply.github.com>
2023-03-21 18:33:29 +0400

    Add files via upload

---
+++ 115.py
@@ -0,0 +1,5 @@
+import os
+source = '/Users/avanitiwari/Desktop/PythonPrograms/115/main.txt'
+dest = '/Users/avanitiwari/Desktop/PythonPrograms/115/new.txt'
+os.rename(source,dest)
+print('file has been renamed successfully')
